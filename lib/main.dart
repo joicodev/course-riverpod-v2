91fcb2334673782855e73a9feb9db6a24f9c8a65
Roimer Peraza <joisephdev@gmail.com>
2023-09-21 21:11:29 -0500

    update main info

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:riverpod_app/config/config.dart';
 
-void main() => runApp(const MyApp());
+void main() => runApp(const ProviderScope(child: MyApp()));
 
 class MyApp extends ConsumerWidget {
   const MyApp({super.key});
