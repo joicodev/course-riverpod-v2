39b24e17f965f0e3aa6b72b8f6a717158d274132
Roimer Peraza <roimerj63@gmail.com>
2023-09-21 20:32:59 -0500

    update app router

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:riverpod_app/config/config.dart';
 
 void main() => runApp(const MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends ConsumerWidget {
   const MyApp({super.key});
 
   @override
-  Widget build(BuildContext context) {
-
+  Widget build(BuildContext context, WidgetRef ref) {
     return MaterialApp.router(
       title: 'Riverpod Providers',
       debugShowCheckedModeBanner: false,
-      routerConfig: appRouter,
-      theme: AppTheme( isDarkmode: false ).getTheme(),
+      routerConfig: ref.watch(appRouterProvider),
+      theme: AppTheme(isDarkmode: false).getTheme(),
     );
   }
 }
